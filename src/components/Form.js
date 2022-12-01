09cf8c7f2370c033a866643923259d69548d7fd2
mohamedaminenadim <mimidamine2nadim@gmail.com>
2022-12-01 09:07:49 +0100

    feat: Added Form component

---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Form extends React.Component{
+    render(){
+        return(
+            <form className="Form">
+                <label>Texte</label>
+                <input type="text" defaultvalue="" name="lastname"/>
+                <input type="submit" value="Envoyer"/>
+            </form>
+        );
+    }
+
+}
+
+export default Form;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,33 @@
 import React from "react";
+import Form from "../components/Form";
 
 /**
  * La classe contact étend de React.Component
  * d'où l'utilisation du render()
  */
 
+ function ListItem(props){
+    return (
+    <li className={props.key}>
+        {props.value}
+    </li>)
+}
+
 class Contact extends React.Component {
+    constructor(props){
+        super(props);
+        const number = [1,3,5,2,7]
+        this.listItem = number.map(
+            (number) => <ListItem
+                key={number.toString()}
+                value={number}/>
+        )}
+
     render() {
         return (
             <div className="Contact">
                 Votre message ...
+                <Form/>
             </div>
         )
     }
